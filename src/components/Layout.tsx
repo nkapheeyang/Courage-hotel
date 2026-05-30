198e31fdd72ff6e452cbdfd48a412bd188fa942c
nkapheeyang <153599661+nkapheeyang@users.noreply.github.com>
2026-05-30 19:52:05 +0100

    feat: Create Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import { Header } from './Header';
+import { Footer } from './Footer';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen bg-cream">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+};
